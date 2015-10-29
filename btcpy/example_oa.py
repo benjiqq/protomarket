b35d9a4d72414a224fd51c27a63cc455b5a6f2e8
benjyz <benjamin.l.cordes@gmail.com>
2015-10-29 19:09:35 +0100

    cleanup

--- btcpy/example_oa.py
+++ btcpy/example_oa.py
@@ -1,16 +1,23 @@
+"""
+simpe example to understand script of an OpenAsset transaction
+"""
+
 from scantx import *
 
+#example transactions selected by hand via coinprism
 issuetx = 'b25ba9cb3a02e24dc53f6672a25fdc047d243cc442dda5e25d5d1590d9a66969'
 transfertx = '9a0466b49bc91007767caf478d9aa8e91ce04c9d0c0aae3023ed0f9bc7e567bb'
 
-printtx(transfertx)
+printtx(issuetx)
 
 """
-# example tx with op_return
-#tuples of (opcode, data, sop_idx) so that the different possible
-#PUSHDATA encodings can be accurately distinguished
+this is the result:
+tuples of (opcode, data, sop_idx)
+see https://en.bitcoin.it/wiki/Script
+note the 106 op_code that beginning for OP_RETURN
+and the OA marker with 18 bytes
 
-(106, None, 0)
+(106, None, 0
 (18, 'OA\x01\x00\x02\x80\xd6\xa3\xf8\x1b\xe0\x90\x82\xf7\xa4\xeb\x02\x00', 1)
 (118, None, 0)
 (169, None, 1)
--- btcpy/oa_balance.py
+++ btcpy/oa_balance.py
@@ -1,4 +1,14 @@
-""" example to use OA parser """
+"""
+example to use OA parser to track balances
+
+OA protocol:
+
+0x4f 0x41 OA protocol tag
+0x01 0x00 version of the protocol
+0x02 items
+80c8afa025e0e6a5efc0eb0200
+
+"""
 
 from openassets import protocol
 import bitcoin
@@ -6,17 +16,37 @@ import bitcoin.rpc
 from bitcoin.core import b2x, lx
 #import protocol.MarkerOutput as MO
 
-txid = '9a0466b49bc91007767caf478d9aa8e91ce04c9d0c0aae3023ed0f9bc7e567bb'
+#txid = '9a0466b49bc91007767caf478d9aa8e91ce04c9d0c0aae3023ed0f9bc7e567bb'
+txids = [
+'b25ba9cb3a02e24dc53f6672a25fdc047d243cc442dda5e25d5d1590d9a66969',
+'e84107b793ff95efb9dc5a7e3a6500d47ff742f2af86aea69e92ad21d952baa1',
+'6730d1972f4b286aadfb70c134ede8373ec2e2304c292d65d47f6b3bbe381316',
+'25cde5b5cd28d812d441bdf230ab025b9fdcac87acd2592828cd6e400628f854']
 
 proxy = bitcoin.rpc.Proxy()
-txid = lx(txid)
-txes = proxy.getrawtransaction(txid)
 
-for transaction in txes.vout[:1]:
-    mo_payload = protocol.MarkerOutput.parse_script(transaction.scriptPubKey)
-    print (b2x(mo_payload))
-    marker = protocol.MarkerOutput.deserialize_payload(mo_payload)
-    print (marker)
+def showTx(txid):
+    txid = lx(txid)
+    txes = proxy.getrawtransaction(txid)
+
+    for transaction in txes.vout[:1]:
+        mo_payload = protocol.MarkerOutput.parse_script(transaction.scriptPubKey)
+        print (b2x(mo_payload))
+        marker = protocol.MarkerOutput.deserialize_payload(mo_payload)
+        print (marker)
+
+    """
+    output:
+    4f4101000280d6a3f81be09082f7a4eb0200
+    MarkerOutput(asset_quantities=[7500000000, 12482498300000], metadata=b'')
+    """
+
+if __name__=='__main__':
+    for tx in txids:
+        showTx(tx)
+
+
+
 
 """
 def _get_unspent_outputs(self, address, **kwargs):
@@ -37,7 +67,3 @@ def _get_unspent_outputs(self, address, **kwargs):
     yield from cache.commit()
     return result
 """
-
-#if marker_output_payload is not None:
-    # Deserialize the payload as a marker output
-#    marker_output = MarkerOutput.deserialize_payload(marker_output_payload)
